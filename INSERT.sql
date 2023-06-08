e32cca47279bc447cb992dc16840319b5a398d69
MICAEL ANDRÉ CUNHA DIAS <8200383@estg.ipp.pt>
2023-06-08 16:21:55 +0100

    Fix Gender

--- INSERT.sql
+++ INSERT.sql
@@ -1,5 +1,5 @@
-INSERT INTO shop.dbo.Collection (Title, Gender, ProductCount) VALUES (N'Inverno', N'Male', 2);
-INSERT INTO shop.dbo.Customer (FirstName, LastName, Gender, AddressLine1, AddressLine2, PostalCode, Country, Phone, Email, NIF) VALUES (N'ui', N'jn', N'Male', N'jkh', N'kjh', N'4610-812', N'PT', N'936291996', N'8200383@estg.ipp.pt', N'214523778');
+INSERT INTO shop.dbo.Collection (Title, Gender, ProductCount) VALUES (N'Inverno', N'Men', 2);
+INSERT INTO shop.dbo.Customer (FirstName, LastName, Gender, AddressLine1, AddressLine2, PostalCode, Country, Phone, Email, NIF) VALUES (N'ui', N'jn', N'Men', N'Right', N'Left', N'4610-812', N'PT', N'936291996', N'8200383@estg.ipp.pt', N'214523778');
 INSERT INTO shop.dbo.Product (CollectionID, Title, Description, VariantsCount) VALUES (1, N'Saia dome', N'Saia curta dome', 2);
 INSERT INTO shop.dbo.ProductLine (ProductID, Color, Size, WarehouseQuantity, DefectQuantity, StandQuantity, Stand, Price) VALUES (1, N'Rosa', N'L', 2, 1, 1, 9, 25.00);
 INSERT INTO shop.dbo.Seller (FirstName, LastName, NIF, ShiftStart, ShiftEnd, SalesCount) VALUES (N'Sergio', N'Felix', N'261994905',N'10:30:00',N'16:30:00', 3);
