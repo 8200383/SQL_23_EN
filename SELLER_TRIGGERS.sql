c887df9c214fd78983c94c1a20e302792eccaf38
CARLOS FERNANDO TEIXEIRA LEITE <8200377@estg.ipp.pt>
2023-06-09 11:28:50 +0100

    Create a Trigger to decrease the StandQauntity when a sala is made

--- SELLER_TRIGGERS.sql
+++ SELLER_TRIGGERS.sql
@@ -1,4 +1,29 @@
 -- TODO DECREASE STOCK WHEN A SALE IS MADE (CREATE)
+CREATE TRIGGER DECREASE_STOCK_WHEN_SALE_MADE
+ON SaleLine
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @quantitySold INT;
+    DECLARE @productLineID INT;
+    DECLARE @availableQuantity INT;
+
+    SELECT TOP 1 @quantitySold = Quantity, @productLineID = ProductLineID FROM inserted;
+
+    SELECT @availableQuantity = StandQuantity FROM ProductLine WHERE ProductLineID = @productLineID;
+
+    IF (@quantitySold > @availableQuantity)
+    BEGIN
+        -- Raise an error or handle the restriction as per your requirements
+        RAISERROR ('Quantity to be sold exceeds available quantity.', 16, 1);
+        -- Rollback the transaction if necessary
+        RETURN;
+    END
+
+    UPDATE ProductLine
+    SET StandQuantity = StandQuantity - @quantitySold
+    WHERE ProductLineID = @productLineID;
+END;
 -- TODO INCREASE SALES COUNT FOR A GIVEN SELLER (SAME TRIGGER AS ABOVE)
 -- TODO ADD PURCHASES COUNT FOR CUSTOMERS AND A TRIGGER FOR IT
 -- TODO DECREASE STAND STOCK AND INCREASE WAREHOUSE AND DEFECT STOCK WHEN A DEFECT IS INSERTED
