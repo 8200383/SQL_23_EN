c2ec41cd2bce4c3fbf2bd9cab4f84a6f58c07120
CARLOS FERNANDO TEIXEIRA LEITE <8200377@estg.ipp.pt>
2023-06-09 11:59:00 +0100

    ADD INCREASE_SALES_COUNT_WHEN_SALE_MADE

--- SELLER_TRIGGERS.sql
+++ SELLER_TRIGGERS.sql
@@ -1,7 +1,7 @@
 CREATE TRIGGER DECREASE_STOCK_WHEN_SALE_MADE
-ON SaleLine
-AFTER INSERT
-AS
+    ON SaleLine
+    AFTER INSERT
+    AS
 BEGIN
     DECLARE @quantitySold INT;
     DECLARE @productLineID INT;
@@ -12,29 +12,43 @@ BEGIN
     SELECT @availableQuantity = StandQuantity FROM ProductLine WHERE ProductLineID = @productLineID;
 
     IF (@quantitySold > @availableQuantity)
-    BEGIN
-        -- Raise an error or handle the restriction as per your requirements
-        RAISERROR ('Quantity to be sold exceeds available quantity.', 16, 1);
-        RETURN;
-    END
+        BEGIN
+            -- Raise an error or handle the restriction as per your requirements
+            RAISERROR ('Quantity to be sold exceeds available quantity.', 16, 1);
+            RETURN;
+        END
 
     UPDATE ProductLine
     SET StandQuantity = StandQuantity - @quantitySold
     WHERE ProductLineID = @productLineID;
 END;
 
+GO;
+
 CREATE TRIGGER INCREASE_PURCHASES_COUNT_WHEN_SALE_MADE
-ON Sale
-AFTER INSERT
-AS
+    ON Sale
+    AFTER INSERT
+    AS
 BEGIN
     UPDATE Customer
     SET PurchasesCount = PurchasesCount + 1
-    WHERE Customer.CustomerID IN (
-        SELECT DISTINCT Sale.CustomerID
-        FROM Sale
-        INNER JOIN inserted ON Sale.SaleID = inserted.SaleID
-    );
+    WHERE Customer.CustomerID IN (SELECT DISTINCT Sale.CustomerID
+                                  FROM Sale
+                                           INNER JOIN inserted ON Sale.SaleID = inserted.SaleID);
 END;
 
--- TODO INCREASE SALES COUNT FOR A GIVEN SELLER (SAME TRIGGER AS ABOVE)
+GO;
+
+
+CREATE TRIGGER INCREASE_SALES_COUNT_WHEN_SALE_MADE
+    ON Sale
+    AFTER INSERT
+    AS
+BEGIN
+    UPDATE Seller
+    SET SalesCount = Seller.SalesCount + 1
+    WHERE Seller.SellerID IN (SELECT DISTINCT Sale.SellerID
+                              FROM Sale
+                                       INNER JOIN inserted ON Sale.SaleID = inserted.SaleID);
+END;
+
