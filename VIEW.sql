655896d999dc1c3df98f27739a1a40e239f5b7b6
CARLOS FERNANDO TEIXEIRA LEITE <8200377@estg.ipp.pt>
2023-06-09 14:00:22 +0100

    Defect View created but show duplicated product for the same product line

--- VIEW.sql
+++ VIEW.sql
@@ -1,36 +1,36 @@
 CREATE VIEW dbo.Invoicing
 AS
-SELECT DATEPART(YEAR, CreatedAt) AS year,
+SELECT DATEPART(YEAR, CreatedAt)  AS year,
        DATEPART(MONTH, CreatedAt) AS month,
-       SUM(Quantity) AS Quantity,
-       SUM(Quantity * Price) as AmountMade
+       SUM(Quantity)              AS Quantity,
+       SUM(Quantity * Price)      as AmountMade
 FROM Sale
-INNER JOIN SaleLine AS Line on Sale.SaleID = Line.SaleID
-INNER JOIN ProductLine AS P on P.ProductLineID = Line.ProductLineID
+         INNER JOIN SaleLine AS Line on Sale.SaleID = Line.SaleID
+         INNER JOIN ProductLine AS P on P.ProductLineID = Line.ProductLineID
 GROUP BY DATEPART(YEAR, CreatedAt), DATEPART(MONTH, CreatedAt)
 
 CREATE VIEW InvoicingByDay
 AS
-SELECT DATEPART(YEAR, CreatedAt) AS year,
+SELECT DATEPART(YEAR, CreatedAt)  AS year,
        DATEPART(MONTH, CreatedAt) AS month,
-       DATEPART(DAY , CreatedAt) AS day,
-       SUM(Quantity) AS Quantity,
-       SUM(Quantity * Price) as AmountMade
+       DATEPART(DAY, CreatedAt)   AS day,
+       SUM(Quantity)              AS Quantity,
+       SUM(Quantity * Price)      as AmountMade
 FROM Sale
-INNER JOIN SaleLine AS Line on Sale.SaleID = Line.SaleID
-INNER JOIN ProductLine AS P on P.ProductLineID = Line.ProductLineID
-GROUP BY DATEPART(YEAR, CreatedAt), DATEPART(MONTH, CreatedAt), DATEPART(DAY , CreatedAt)
+         INNER JOIN SaleLine AS Line on Sale.SaleID = Line.SaleID
+         INNER JOIN ProductLine AS P on P.ProductLineID = Line.ProductLineID
+GROUP BY DATEPART(YEAR, CreatedAt), DATEPART(MONTH, CreatedAt), DATEPART(DAY, CreatedAt)
 
 CREATE VIEW InvoicingByWeek
 AS
 SELECT DATEPART(YEAR, CreatedAt) AS year,
        DATEPART(WEEK, CreatedAt) AS week,
-       SUM(Quantity) AS Quantity,
-       SUM(Quantity * Price) as AmountMade
+       SUM(Quantity)             AS Quantity,
+       SUM(Quantity * Price)     as AmountMade
 FROM Sale
-INNER JOIN SaleLine AS Line on Sale.SaleID = Line.SaleID
-INNER JOIN ProductLine AS P on P.ProductLineID = Line.ProductLineID
-GROUP BY DATEPART(YEAR, CreatedAt), DATEPART(WEEK , CreatedAt)
+         INNER JOIN SaleLine AS Line on Sale.SaleID = Line.SaleID
+         INNER JOIN ProductLine AS P on P.ProductLineID = Line.ProductLineID
+GROUP BY DATEPART(YEAR, CreatedAt), DATEPART(WEEK, CreatedAt)
 
 CREATE VIEW SalesByDay
 AS
@@ -47,63 +47,61 @@ GROUP BY Sale.CreatedAt, ProductLine.Size, ProductLine.Color, Product.Title
 
 CREATE VIEW RevenueBySeller
 AS
-SELECT
-    Seller.SellerID,
-    CONCAT(Seller.FirstName, ' ', Seller.LastName) AS SellerName,
-    MONTH(Sale.CreatedAt) as Month,
-    YEAR(Sale.CreatedAt) as Year,
-    SUM(SaleLine.Quantity) AS Quantity,
-    SUM(SaleLine.Quantity * ProductLine.Price) as Revenue
+SELECT Seller.SellerID,
+       CONCAT(Seller.FirstName, ' ', Seller.LastName) AS SellerName,
+       MONTH(Sale.CreatedAt)                          as Month,
+       YEAR(Sale.CreatedAt)                           as Year,
+       SUM(SaleLine.Quantity)                         AS Quantity,
+       SUM(SaleLine.Quantity * ProductLine.Price)     as Revenue
 FROM Seller
-    INNER JOIN Sale on Sale.SellerID = Seller.SellerID
-    INNER JOIN SaleLine on Sale.SaleID = SaleLine.SaleID
-    INNER JOIN ProductLine on ProductLine.ProductLineID = SaleLine.ProductLineID
+         INNER JOIN Sale on Sale.SellerID = Seller.SellerID
+         INNER JOIN SaleLine on Sale.SaleID = SaleLine.SaleID
+         INNER JOIN ProductLine on ProductLine.ProductLineID = SaleLine.ProductLineID
 GROUP BY Seller.SellerID, Sale.CreatedAt, Seller.FirstName, Seller.LastName
-ORDER BY Sale.CreatedAt, Revenue DESC OFFSET 0 ROWS
+ORDER BY Sale.CreatedAt, Revenue DESC
+OFFSET 0 ROWS
 
 CREATE VIEW TopProducts
 AS
-SELECT
-    ProductLine.ProductID,
-    Product.Title,
-    ProductLine.Size,
-    ProductLine.Color,
-    ProductLine.Price,
-    COUNT(SL.Quantity) AS Quantity
-    FROM ProductLine
-    INNER JOIN Product on Product.ProductID = ProductLine.ProductID
-    INNER JOIN SaleLine SL on ProductLine.ProductLineID = SL.ProductLineID
+SELECT ProductLine.ProductID,
+       Product.Title,
+       ProductLine.Size,
+       ProductLine.Color,
+       ProductLine.Price,
+       COUNT(SL.Quantity) AS Quantity
+FROM ProductLine
+         INNER JOIN Product on Product.ProductID = ProductLine.ProductID
+         INNER JOIN SaleLine SL on ProductLine.ProductLineID = SL.ProductLineID
 GROUP BY ProductLine.ProductID, ProductLine.Price, ProductLine.Color, ProductLine.Size, Product.Title
-ORDER BY Quantity DESC OFFSET 0 ROWS
+ORDER BY Quantity DESC
+OFFSET 0 ROWS
 
-/* TODO Defect View */
-/*
-CREATE VIEW FaultyClothesAmount
+-- TODO See why it's output is a duplicated product for the same product line
+CREATE VIEW DefectView
 AS
-SELECT
-    DATEPART(YEAR, CreatedAt) AS year,
-    DATEPART(WEEK, CreatedAt) AS week,
-    Report.CreatedAt,
-    ProductLine.ProductID,
-    SUM(Report.Quantity) AS Quantity,
-    SUM(Report.Quantity * ProductLine.Price) as Amount
-FROM Report
-    INNER JOIN ProductLine on ProductLine.ProductLineID = Report.ProductLineID
-GROUP by ProductLine.ProductID, Report.CreatedAt
-*/
+SELECT DATEPART(YEAR, Defect.CreatedAt)                    AS Year,
+       DATEPART(WEEK, Defect.CreatedAt)                    AS Week,
+       Product.Title,
+       Product.Description,
+       SUM(ProductLine.DefectQuantity) AS Quantity,
+       SUM(ProductLine.DefectQuantity * ProductLine.Price) AS Amount
+FROM Defect
+         INNER JOIN ProductLine ON ProductLine.ProductLineID = Defect.ProductLineID
+         INNER JOIN Product ON Product.ProductID = ProductLine.ProductID
+GROUP BY Defect.CreatedAt, Product.Title, Product.Description;
 
 CREATE VIEW Invoice
 AS
 SELECT Customer.NIF,
-        Sale.SaleID,
-        Product.Title,
-        ProductLine.Color,
-        ProductLine.Size,
-        SUM(ProductLine.ProductLineID) as Quantity,
-        SUM(ProductLine.ProductLineID * ProductLine.Price) as Amount
+       Sale.SaleID,
+       Product.Title,
+       ProductLine.Color,
+       ProductLine.Size,
+       SUM(ProductLine.ProductLineID)                     as Quantity,
+       SUM(ProductLine.ProductLineID * ProductLine.Price) as Amount
 FROM Customer
-    INNER JOIN Sale ON Sale.CustomerID = Customer.CustomerID
-    INNER JOIN SaleLine ON SaleLine.SaleID = Sale.SaleID
-    INNER JOIN ProductLine ON ProductLine.ProductLineID = SaleLine.ProductLineID
-    INNER JOIN Product ON Product.ProductID = ProductLine.ProductID
+         INNER JOIN Sale ON Sale.CustomerID = Customer.CustomerID
+         INNER JOIN SaleLine ON SaleLine.SaleID = Sale.SaleID
+         INNER JOIN ProductLine ON ProductLine.ProductLineID = SaleLine.ProductLineID
+         INNER JOIN Product ON Product.ProductID = ProductLine.ProductID
 GROUP by Customer.NIF, Sale.SaleID, Product.Title, ProductLine.Color, ProductLine.Size
