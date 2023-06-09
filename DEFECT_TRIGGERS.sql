8c5a198fd44563cb353b2143292ea52d6d0176f1
MICAEL ANDRÉ CUNHA DIAS <8200383@estg.ipp.pt>
2023-06-09 11:26:43 +0100

    Add Defect Trigger

---
+++ DEFECT_TRIGGERS.sql
@@ -0,0 +1,10 @@
+CREATE TRIGGER UPDATE_STOCK_WHEN_INSERT_DEFECT
+    ON Defect
+    AFTER INSERT
+    AS
+BEGIN
+    UPDATE ProductLine
+    SET StandQuantity = StandQuantity - 1,
+        DefectQuantity = DefectQuantity + 1
+    WHERE ProductID IN (SELECT ProductID FROM inserted)
+END;
